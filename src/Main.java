071257a61035cd2e3d0b3f012964736592f14b00
stasnnnnnn <stas_ya@list.ru>
2020-04-07 22:55:05 +0300

    bug fixes

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,13 @@ class Entity  {
     public float tx = 0;
     public float ty = 0;
     public float health = 1f;
-    public float speed = 0.5f;
+    public float maxSpeed = 0.5f;
+    public float currentSpeed = maxSpeed;
     public float force = 0.5f;
     public float agility = 0.5f;
     public float stamina = 0.5f;
     public boolean useStamina = false;
     public float predator = 0f;
-    public float accumulatedStamina = 0f;
 
 
     public Entity (float x, float y) {
@@ -27,7 +27,7 @@ public class Main {
     final int W = 800;
     final int H = 800;
     float sumForce;
-    float sumSpeed;
+    float sumMaxSpeed;
     float sumAgility;
     float sumStamina;
     float sumPredator;
@@ -35,9 +35,10 @@ public class Main {
     final float sightDistance = 100f;
     final float directionChangeRate = 0.01f;
     final float radiusEntity = 10f;
-    final float chanceMutation = 0.0001f;
+    final float chanceMutation = 0.1f;
     final float coefHealthChange = 0.00015f;
     final float coefHealthIncrease = 0.001f;
+    final float coefMutation = 0.1f;
     ArrayList<Entity> entities = new ArrayList<>();
     int countCycles = 0;
 
@@ -91,7 +92,7 @@ public class Main {
             g2.setColor(new Color(255, 0, 0, 255));
             g2.drawString("Average force: " + Float.toString(sumForce / entities.size()), 50, 150);
             g2.setColor(new Color(0, 255, 0, 255));
-            g2.drawString("Average speed: " + Float.toString(sumSpeed / entities.size()), 50, 200);
+            g2.drawString("Average max speed: " + Float.toString(sumMaxSpeed / entities.size()), 50, 200);
             g2.setColor(new Color(0, 0, 255, 255));
             g2.drawString("Average stamina: " + Float.toString(sumStamina / entities.size()), 50, 250);
             g2.setColor(new Color(0, 0, 0, 255));
@@ -102,7 +103,7 @@ public class Main {
                     g2.setColor(new Color(0, 0, 0, 255));
                     g2.drawOval((int) (a.x - radiusEntity * Math.sqrt(a.health)), (int) (a.y - radiusEntity * Math.sqrt(a.health)), (int) (radiusEntity * Math.sqrt(a.health) * 2), (int) (radiusEntity * Math.sqrt(a.health)) * 2);
                 }
-                g2.setColor(new Color(Math.round(a.force * 255), Math.round(a.speed * 255), Math.round(a.stamina * 255), 130));
+                g2.setColor(new Color(Math.round(a.force * 255), Math.round(a.maxSpeed * 255), Math.round(a.stamina * 255), 130));
                 g2.fillOval((int) (a.x - radiusEntity * Math.sqrt(a.health)), (int) (a.y - radiusEntity * Math.sqrt(a.health)), (int) (radiusEntity * Math.sqrt(a.health) * 2), (int) (radiusEntity * Math.sqrt(a.health)) * 2);
             }
         }
@@ -110,160 +111,161 @@ public class Main {
         private void logic() {
             countCycles++;
             sumForce = 0;
-            sumSpeed = 0;
+            sumMaxSpeed = 0;
             sumStamina = 0;
             sumAgility = 0;
             sumPredator = 0;
-            for (int i = 0; i < entities.size(); i++) {
-                Entity a = entities.get(i);
-                double targetAngle = Math.atan2(a.ty, a.tx);
-                if (a.useStamina & a.accumulatedStamina > ((1 / (1 - a.stamina)) - 1) * 128) {
-                    a.x += (float) Math.cos(targetAngle) * a.speed;
-                    a.y += (float) Math.sin(targetAngle) * a.speed;
-                    a.accumulatedStamina--;
+            for (int currentEntityIndex = 0; currentEntityIndex < entities.size(); currentEntityIndex++) {
+                Entity currentEntity = entities.get(currentEntityIndex);
+                double targetAngle = Math.atan2(currentEntity.ty, currentEntity.tx);
+
+                if (currentEntity.useStamina) {
+                    currentEntity.x += (float) Math.cos(targetAngle) * currentEntity.currentSpeed;
+                    currentEntity.y += (float) Math.sin(targetAngle) * currentEntity.currentSpeed;
+                    currentEntity.currentSpeed *= 1 - currentEntity.stamina / 1000;
                 } else {
-                    a.x += (float) Math.cos(targetAngle) * a.speed / 2;
-                    a.y += (float) Math.sin(targetAngle) * a.speed / 2;
-                    if (a.accumulatedStamina < ((1 / (1 - a.stamina)) - 1) * 1024)
-                        a.accumulatedStamina += a.stamina;
+                    currentEntity.x += (float) Math.cos(targetAngle) * currentEntity.currentSpeed / 4;
+                    currentEntity.y += (float) Math.sin(targetAngle) * currentEntity.currentSpeed / 4;
+                    currentEntity.currentSpeed /= 1 - currentEntity.stamina / 1000;
+                    if (currentEntity.currentSpeed > currentEntity.maxSpeed)
+                        currentEntity.currentSpeed = currentEntity.maxSpeed;
                 }
-                if (a.x < 0) a.x = W;
-                else if (a.x > W) a.x = 0;
-                if (a.y < 0) a.y = H;
-                else if (a.y > H) a.y = 0;
+                if (currentEntity.x < 0) currentEntity.x = W;
+                else if (currentEntity.x > W) currentEntity.x = 0;
+                if (currentEntity.y < 0) currentEntity.y = H;
+                else if (currentEntity.y > H) currentEntity.y = 0;
                 Entity closestFood = null;
                 int indexClosestFood = 0;
                 float minFoodDist = (W * W) + (H * H);
-                for (int j = 0; j < entities.size(); j++) {
-                    Entity b = entities.get(j);
-                    if (a.predator == 0f) continue;
-                    if (b.force >= a.force) continue;
-                    if (b.speed >= a.speed & a.accumulatedStamina < ((1 / (1 - a.stamina)) - 1) * 128) continue;
-                    float dist1 = (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
-                    float dist2 = (W - Math.abs(a.x - b.x)) * (W - Math.abs(a.x - b.x)) + (a.y - b.y) * (a.y - b.y);
-                    float dist3 = (a.x - b.x) * (a.x - b.x) + (H - Math.abs(a.y - b.y)) * (H - Math.abs(a.y - b.y));
-                    float dist4 = (W - Math.abs(a.x - b.x)) * (W - Math.abs(a.x - b.x)) + (H - Math.abs(a.y - b.y)) * (H - Math.abs(a.y - b.y));
+                for (int i = 0; i < entities.size(); i++) {
+                    Entity e = entities.get(i);
+                    if (currentEntity.predator == 0f) continue;
+                    if (e.force >= currentEntity.force) continue;
+                    float dist1 = (currentEntity.x - e.x) * (currentEntity.x - e.x) + (currentEntity.y - e.y) * (currentEntity.y - e.y);
+                    float dist2 = (W - Math.abs(currentEntity.x - e.x)) * (W - Math.abs(currentEntity.x - e.x)) + (currentEntity.y - e.y) * (currentEntity.y - e.y);
+                    float dist3 = (currentEntity.x - e.x) * (currentEntity.x - e.x) + (H - Math.abs(currentEntity.y - e.y)) * (H - Math.abs(currentEntity.y - e.y));
+                    float dist4 = (W - Math.abs(currentEntity.x - e.x)) * (W - Math.abs(currentEntity.x - e.x)) + (H - Math.abs(currentEntity.y - e.y)) * (H - Math.abs(currentEntity.y - e.y));
                     float dist = Math.min(Math.min(dist1, dist2), Math.min(dist3, dist4));
                     if (dist < sightDistance * sightDistance) {
                         if (dist < minFoodDist) {
                             minFoodDist = dist;
-                            closestFood = b;
-                            indexClosestFood = j;
+                            closestFood = e;
+                            indexClosestFood = i;
                         }
                     }
                 }
                 Entity closestEnemy = null;
                 float minEnemyDist = (W * W) + (H * H);
-                for (int j = 0; j < entities.size(); j++) {
-                    Entity b = entities.get(j);
-                    if (b.predator == 0f) continue;
-                    if (b.force <= a.force) continue;
-                    if (b.speed <= a.speed & b.accumulatedStamina < ((1 / (1 - a.stamina)) - 1) * 128) continue;
-                    float dist1 = (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
-                    float dist2 = (W - Math.abs(a.x - b.x)) * (W - Math.abs(a.x - b.x)) + (a.y - b.y) * (a.y - b.y);
-                    float dist3 = (a.x - b.x) * (a.x - b.x) + (H - Math.abs(a.y - b.y)) * (H - Math.abs(a.y - b.y));
-                    float dist4 = (W - Math.abs(a.x - b.x)) * (W - Math.abs(a.x - b.x)) + (H - Math.abs(a.y - b.y)) * (H - Math.abs(a.y - b.y));
+                for (int i = 0; i < entities.size(); i++) {
+                    Entity e = entities.get(i);
+                    if (e.predator == 0f) continue;
+                    if (e.force <= currentEntity.force) continue;
+                    float dist1 = (currentEntity.x - e.x) * (currentEntity.x - e.x) + (currentEntity.y - e.y) * (currentEntity.y - e.y);
+                    float dist2 = (W - Math.abs(currentEntity.x - e.x)) * (W - Math.abs(currentEntity.x - e.x)) + (currentEntity.y - e.y) * (currentEntity.y - e.y);
+                    float dist3 = (currentEntity.x - e.x) * (currentEntity.x - e.x) + (H - Math.abs(currentEntity.y - e.y)) * (H - Math.abs(currentEntity.y - e.y));
+                    float dist4 = (W - Math.abs(currentEntity.x - e.x)) * (W - Math.abs(currentEntity.x - e.x)) + (H - Math.abs(currentEntity.y - e.y)) * (H - Math.abs(currentEntity.y - e.y));
                     float dist = Math.min(Math.min(dist1, dist2), Math.min(dist3, dist4));
                     if (dist < sightDistance * sightDistance) {
                         if (dist < minEnemyDist) {
                             minEnemyDist = dist;
-                            closestEnemy = b;
+                            closestEnemy = e;
                         }
                     }
                 }
                 if (minFoodDist > minEnemyDist & closestEnemy != null) {
-                    a.useStamina = true;
-                    if (Math.abs(closestEnemy.x - a.x) > sightDistance)
-                        a.tx = closestEnemy.x - a.x;
+                    currentEntity.useStamina = true;
+                    if (Math.abs(closestEnemy.x - currentEntity.x) > sightDistance)
+                        currentEntity.tx = closestEnemy.x - currentEntity.x;
                     else
-                        a.tx = -closestEnemy.x + a.x;
-                    if (Math.abs(closestEnemy.y - a.y) > sightDistance)
-                        a.ty = closestEnemy.y - a.y;
+                        currentEntity.tx = -closestEnemy.x + currentEntity.x;
+                    if (Math.abs(closestEnemy.y - currentEntity.y) > sightDistance)
+                        currentEntity.ty = closestEnemy.y - currentEntity.y;
                     else
-                        a.ty = -closestEnemy.y + a.y;
+                        currentEntity.ty = -closestEnemy.y + currentEntity.y;
                 } else {
-                    a.useStamina = true;
+                    currentEntity.useStamina = true;
                     if (closestFood != null) {
-                        if (Math.abs(closestFood.x - a.x) > sightDistance)
-                            a.tx = -closestFood.x + a.x;
+                        if (Math.abs(closestFood.x - currentEntity.x) > sightDistance)
+                            currentEntity.tx = -closestFood.x + currentEntity.x;
                         else
-                            a.tx = closestFood.x - a.x;
-                        if (Math.abs(closestFood.y - a.y) > sightDistance)
-                            a.ty = -closestFood.y + a.y;
+                            currentEntity.tx = closestFood.x - currentEntity.x;
+                        if (Math.abs(closestFood.y - currentEntity.y) > sightDistance)
+                            currentEntity.ty = -closestFood.y + currentEntity.y;
                         else
-                            a.ty = closestFood.y - a.y;
+                            currentEntity.ty = closestFood.y - currentEntity.y;
                         if (minFoodDist < radiusEntity * radiusEntity) {
-                            if (closestFood.agility / 2 + closestFood.force > a.agility / 2 + a.force) {
-                                closestFood.health *= 1 - ((a.agility / 2 + a.force) / (closestFood.agility / 2 + closestFood.force)) * ((a.agility / 2 + a.force) / (closestFood.agility / 2 + closestFood.force));
-                                closestFood.health += a.health;
-                                entities.remove(i);
-                                i--;
+                            if (closestFood.agility + closestFood.force > currentEntity.agility + currentEntity.force) {
+                                closestFood.health *= 1 - ((currentEntity.agility + currentEntity.force) / (closestFood.agility + closestFood.force)) * ((currentEntity.agility + currentEntity.force) / (closestFood.agility + closestFood.force));
+                                closestFood.health += currentEntity.health;
+                                entities.remove(currentEntityIndex);
+                                currentEntityIndex--;
                             } else {
-                                a.health *= 1 - (closestFood.force / a.force) * (closestFood.force / a.force);
-                                a.health += closestFood.health;
+                                currentEntity.health *= 1 - (closestFood.force / currentEntity.force) * (closestFood.force / currentEntity.force);
+                                currentEntity.health += closestFood.health;
                                 entities.remove(indexClosestFood);
-                                if (indexClosestFood < i)
-                                    i--;
+                                if (indexClosestFood < currentEntityIndex)
+                                    currentEntityIndex--;
                             }
                         }
                     } else {
-                        a.useStamina = false;
+                        currentEntity.useStamina = false;
                         if (Math.random() < directionChangeRate) {
                             double randomAngle = Math.random() * Math.PI * 2;
-                            a.tx = (float) Math.cos(randomAngle) * 2;
-                            a.ty = (float) Math.sin(randomAngle) * 2;
+                            currentEntity.tx = (float) Math.cos(randomAngle) * 2;
+                            currentEntity.ty = (float) Math.sin(randomAngle) * 2;
                         }
                     }
                 }
-                a.health += coefHealthIncrease;
-                if (a.health >= maxHealth) {
-                    a.health = 1f;
-                    Entity b = new Entity(a.x + (float) Math.random() * 10 - 5, a.y + (float) Math.random() * 10 - 5);
-                    b.speed = a.speed;
-                    b.force = a.force;
-                    b.agility = a.agility;
-                    b.stamina = a.stamina;
-                    b.predator = a.predator;
-                    entities.add(b);
-                }
-                if (Math.random() < chanceMutation) {
-                    a.health = 1f;
-                    a.accumulatedStamina = 0f;
-                    a.speed = (float) (a.speed * (Math.random() / 5 + 0.9f));
-                    if (a.speed >= 1)
-                        a.speed = 0.999999f;
-                    a.force = (float) (a.force * (Math.random() / 5 + 0.9f));
-                    if (a.force >= 1)
-                        a.force = 0.999999f;
-                    a.agility = (float) (a.agility * (Math.random() / 5 + 0.9f));
-                    if (a.agility >= 1)
-                        a.agility = 0.999999f;
-                    a.stamina = (float) (a.stamina * (Math.random() / 5 + 0.9f));
-                    if (a.stamina >= 1)
-                        a.stamina = 0.999999f;
-                    if ((Math.random() < 0.1f)) {
-                        if (a.predator == 0f)
-                            a.predator = 1f;
+                currentEntity.health += coefHealthIncrease;
+                if (currentEntity.health >= maxHealth) {
+                    currentEntity.health = 1f;
+                    Entity newEntity = new Entity(currentEntity.x + (float) Math.random() * 10 - 5, currentEntity.y + (float) Math.random() * 10 - 5);
+                    newEntity.maxSpeed = currentEntity.maxSpeed;
+                    newEntity.force = currentEntity.force;
+                    newEntity.agility = currentEntity.agility;
+                    newEntity.stamina = currentEntity.stamina;
+                    newEntity.predator = currentEntity.predator;
+                    if (Math.random() < chanceMutation) {
+                        newEntity.health = 1f;
+                        if (Math.random() < 0.5f)
+                            newEntity.maxSpeed = newEntity.maxSpeed * (1 - coefMutation);
                         else
-                            a.predator = 0f;
+                            newEntity.maxSpeed = newEntity.maxSpeed + (1 - newEntity.maxSpeed) * coefMutation;
+                        if (Math.random() < 0.5f)
+                            newEntity.force = newEntity.force * (1 - coefMutation);
+                        else
+                            newEntity.force = newEntity.force + (1 - newEntity.force) * coefMutation;
+                        if (Math.random() < 0.5f)
+                            newEntity.agility = newEntity.agility * (1 - coefMutation);
+                        else
+                            newEntity.agility = newEntity.agility + (1 - newEntity.agility) * coefMutation;
+                        if (Math.random() < 0.5f)
+                            newEntity.stamina = newEntity.stamina * (1 - coefMutation);
+                        else
+                            newEntity.stamina = newEntity.stamina + (1 - newEntity.stamina) * coefMutation;
+                        if ((Math.random() < 0.5f)) {
+                            if (newEntity.predator == 0f)
+                                newEntity.predator = 1f;
+                            else
+                                newEntity.predator = 0f;
+                        }
                     }
-
+                    entities.add(newEntity);
                 }
-                a.health -= ((1 / (1 - a.speed)) - 1) * coefHealthChange;
-                a.health -= ((1 / (1 - a.agility)) - 1) * coefHealthChange;
-                a.health -= ((1 / (1 - a.force)) - 1) * coefHealthChange;
-                a.health -= ((1 / (1 - a.stamina)) - 1) * coefHealthChange;
-                a.health -= a.predator * coefHealthChange;
-
-                if (a.health <= 0) {
-                    entities.remove(i);
-                    i--;
+                currentEntity.health -= ((1 / (1 - currentEntity.maxSpeed)) - 1) * coefHealthChange;
+                currentEntity.health -= ((1 / (1 - currentEntity.agility)) - 1) * coefHealthChange;
+                currentEntity.health -= ((1 / (1 - currentEntity.force)) - 1) * coefHealthChange;
+                currentEntity.health -= ((1 / (1 - currentEntity.stamina)) - 1) * coefHealthChange;
+                //currentEntity.health -= currentEntity.predator * coefHealthChange;
+                if (currentEntity.health <= 0) {
+                    entities.remove(currentEntityIndex);
+                    currentEntityIndex--;
                 }
-                sumForce += a.force;
-                sumSpeed += a.speed;
-                sumStamina += a.stamina;
-                sumAgility += a.agility;
-                sumPredator += a.predator;
+                sumForce += currentEntity.force;
+                sumMaxSpeed += currentEntity.maxSpeed;
+                sumStamina += currentEntity.stamina;
+                sumAgility += currentEntity.agility;
+                sumPredator += currentEntity.predator;
             }
         }
     }
